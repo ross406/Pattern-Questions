4280a97b639c177e1bae9852d88c449e359bec84
Roshan sharma <roshansharma406@gmail.com>
2020-10-21 13:35:41 +0530

    new patterns added

--- app.js
+++ app.js
@@ -381,4 +381,342 @@
 // pattern(5);
 
 
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
 
+
+
+// let string = '';
+
+// function pattern(num) {
+ 
+
+//   for(let i=num; i>=1; i--) {
+   
+   
+//     for(let j=1; j<=i; j++) {
+//      string+=j;
+//      string+=' ';
+     
+      
+//     }
+//     string+='\n';
+//   } 
+
+//   console.log(string);
+// }
+
+// pattern(5);
+
+
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+// let string = '';
+
+// function pattern(num) {
+ 
+
+//   for(let i=1; i<=num; i++) {
+   
+   
+//     for(let j=1; j<=num+1-i; j++) {
+//      string+=j;
+//      string+=' ';
+     
+      
+//     }
+//     string+='\n';
+//   } 
+
+//   console.log(string);
+// }
+
+// pattern(5);
+
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+
+
+// let string = '';
+
+// function pattern(num) {
+ 
+//   let n = 1;
+
+//   for(let i=1; i<=num; i++) {
+   
+   
+//     for(let j=1; j<=i; j++) {
+   
+//     if((i+j) % 2 === 0) {
+//       string+='1'
+//     } else {
+//       string+='0'      
+//     }    
+//     string+=' ';      
+//     }
+//     string+='\n';
+//   } 
+
+//   console.log(string);
+// }
+
+// pattern(5);
+
+//     # # # # #
+//    # # # # #
+//   # # # # #
+//  # # # # #
+// # # # # #
+
+
+// let string = '';
+
+// function pattern(num) {
+ 
+
+
+//   for(let i=1; i<=num; i++) {
+   
+//     for(let j=1; j<=num-i; j++) {
+//       string+=' ';
+//     }
+   
+//     for(let j=1; j<=num; j++) {
+      
+//        string+="# "
+   
+//     }
+//     string+='\n';
+//   } 
+
+//   console.log(string);
+// }
+
+// pattern(5);
+
+
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// let string = '';
+
+// function pattern(num) {
+ 
+
+//   for(let i=1; i<=num; i++) {
+//     for(let j=1; j<=num-i; j++) {
+//             string+=' ';
+//           }
+   
+//     for(let j=1; j<=i; j++) {
+//      string+=j;
+//      string+=' ';
+      
+      
+//     }
+//     string+='\n';
+//   } 
+
+//   console.log(string);
+// }
+
+// pattern(5);
+
+// Palindrome
+
+  //         1
+  //       2 1 2
+  //     3 2 1 2 3
+  //   4 3 2 1 2 3 4
+  // 5 4 3 2 1 2 3 4 5
+
+
+// let string = '';
+
+// function pattern(num) {
+
+//   for(let i=1; i<=num; i++) {
+//     let j;
+
+//     for(j=1; j<=num-i; j++){
+//       string+='  ';
+//     }
+
+//     let k=i;
+
+//     for(; j<=num;j++){
+//         string+=k--;
+//         string+=' ';
+//     }
+
+//     k=2;
+
+//     for(; j<=num+i-1;j++){
+//       string+=k++;
+//       string+=' ';
+//     }
+
+//     string+='\n'
+
+//   }
+
+//   console.log(string)
+
+// }
+
+// pattern(5);
+
+    //       #
+    //     # # #
+    //   # # # # #
+    // # # # # # # #
+    // # # # # # # #
+    //   # # # # #
+    //     # # #
+    //       #
+
+
+// let string = '';
+
+// function pattern(num){
+
+//   for(let i=1; i<=num; i++){
+//     for(let j=1; j<=num-i; j++) {
+//       string+="  ";
+//     }
+//     for(let j=1; j<=i*2-1; j++) {
+//       string+="# ";
+//     }
+//     string+='\n';
+//   }
+
+//   for(let i=num; i>=1; i--){
+//     for(let j=1; j<=num-i; j++) {
+//       string+="  ";
+//     }
+//     for(let j=1; j<=i*2-1; j++) {
+//       string+="# ";
+//     }
+//     string+='\n';
+//   }
+
+//   console.log(string);
+// }
+
+// pattern(4);
+
+
+  //       #
+  //     # #
+  //   # # #
+  // # # # #
+  // # # # #
+  //   # # #
+  //     # #
+  //       #
+
+
+// let string = '';
+
+// function pattern(num){
+
+//   for(let i=1; i<=num; i++){
+//     for(let j=1; j<=num-i; j++) {
+//       string+="  ";
+//     }
+//     for(let j=1; j<=i; j++) {
+//       string+="# ";
+//     }
+//     string+='\n';
+//   }
+
+//   for(let i=num; i>=1; i--){
+//     for(let j=1; j<=num-i; j++) {
+//       string+="  ";
+//     }
+//     for(let j=1; j<=i; j++) {
+//       string+="# ";
+//     }
+//     string+='\n';
+//   }
+
+//   console.log(string);
+// }
+
+// pattern(4);
+
+    //     #
+    //    # #
+    //   # # #
+    //  # # # #
+    //  # # # #
+    //   # # #
+    //    # #
+    //     #
+
+// let string = '';
+
+// function pattern(num){
+
+//   for(let i=1; i<=num; i++){
+//     for(let j=1; j<=num-i; j++) {
+//       string+=" ";
+//     }
+//     for(let j=1; j<=i; j++) {
+//       string+="# ";
+//     }
+//     string+='\n';
+//   }
+
+//   for(let i=num; i>=1; i--){
+//     for(let j=1; j<=num-i; j++) {
+//       string+=" ";
+//     }
+//     for(let j=1; j<=i; j++) {
+//       string+="# ";
+//     }
+//     string+='\n';
+//   }
+
+//   console.log(string);
+// }
+
+// pattern(4);
+
+
+
+const pattern = (num) => {
+
+  let string = '';
+
+  for(let i=1; i<=3; i++) {
+    for(let j=1; j<=num; j++){
+      if(((i+j) % 4 === 0) || (i===2  && j%4 ===0)) {
+        string+="# ";
+      } else {
+        string+="  ";
+      }
+    }
+
+    string+='\n';
+  }
+
+  console.log(string);
+}
+
+pattern(9);
